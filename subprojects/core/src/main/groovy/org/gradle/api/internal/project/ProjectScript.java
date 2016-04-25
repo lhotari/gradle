95e5b9f886bd870b8756d3d7e9a83fc08af4c2d8
Lari Hotari <lari.hotari@gradle.com>
2016-04-25 08:54:36 -0400

    Add methods to script class to reduce dynamic invocation overhead

--- subprojects/core/src/main/groovy/org/gradle/api/internal/project/ProjectScript.java
+++ subprojects/core/src/main/groovy/org/gradle/api/internal/project/ProjectScript.java
@@ -17,16 +17,27 @@
 package org.gradle.api.internal.project;
 
 import groovy.lang.Closure;
+import org.gradle.api.InvalidUserDataException;
 import org.gradle.api.Project;
+import org.gradle.api.Task;
+import org.gradle.api.UnknownProjectException;
+import org.gradle.api.artifacts.ConfigurationContainer;
+import org.gradle.api.artifacts.dsl.DependencyHandler;
+import org.gradle.api.artifacts.dsl.RepositoryHandler;
 import org.gradle.api.initialization.dsl.ScriptHandler;
+import org.gradle.api.invocation.Gradle;
 import org.gradle.api.logging.Logger;
 import org.gradle.api.logging.LoggingManager;
+import org.gradle.api.plugins.ExtensionContainer;
 import org.gradle.groovy.scripts.DefaultScript;
 import org.gradle.internal.logging.StandardOutputCapture;
 
+import java.io.File;
 import java.util.Map;
+import java.util.Set;
 
 public abstract class ProjectScript extends DefaultScript {
+
     public void apply(Closure closure) {
         getProject().apply(closure);
     }
@@ -55,4 +66,91 @@ public abstract class ProjectScript extends DefaultScript {
         return getProject().toString();
     }
 
+    public Project getRootProject() {
+        return getProject().getRootProject();
+    }
+
+    public File getRootDir() {
+        return getProject().getRootDir();
+    }
+
+    public File getBuildDir() {
+        return getProject().getBuildDir();
+    }
+
+    public Set<Project> getAllprojects() {
+        return getProject().getAllprojects();
+    }
+
+    public Set<Project> getSubprojects() {
+        return getProject().getSubprojects();
+    }
+
+    public Task task(String name) throws InvalidUserDataException {
+        return getProject().task(name);
+    }
+
+    public Task task(Map<String, ?> args, String name) throws InvalidUserDataException {
+        return getProject().task(args, name);
+    }
+
+    public Task task(Map<String, ?> args, String name, Closure configureClosure) {
+        return getProject().task(args, name, configureClosure);
+    }
+
+    public Task task(String name, Closure configureClosure) {
+        return getProject().task(name, configureClosure);
+    }
+
+    public Project project(String path) throws UnknownProjectException {
+        return getProject().project(path);
+    }
+
+    public Project project(String path, Closure configureClosure) {
+        return getProject().project(path, configureClosure);
+    }
+
+    public Project findProject(String path) {
+        return getProject().findProject(path);
+    }
+
+    public File getProjectDir() {
+        return getProject().getProjectDir();
+    }
+
+    public ConfigurationContainer getConfigurations() {
+        return getProject().getConfigurations();
+    }
+
+    public void configurations(Closure configureClosure) {
+        getProject().configurations(configureClosure);
+    }
+
+    public RepositoryHandler getRepositories() {
+        return getProject().getRepositories();
+    }
+
+    public void repositories(Closure configureClosure) {
+        getProject().repositories(configureClosure);
+    }
+
+    public DependencyHandler getDependencies() {
+        return getProject().getDependencies();
+    }
+
+    public void dependencies(Closure configureClosure) {
+        getProject().dependencies(configureClosure);
+    }
+
+    public ExtensionContainer getExtensions() {
+        return getProject().getExtensions();
+    }
+
+    public Gradle getGradle() {
+        return getProject().getGradle();
+    }
+
+    public File getBuildFile() {
+        return getProject().getBuildFile();
+    }
 }
